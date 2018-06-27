88f45c634fc2f1df1869ea8b6f06405b39608d00
Dom <dom_dent@hotmail.com>
2018-06-27 10:23:40 +0100

    added comments to each function in the classes

--- firm.py
+++ firm.py
@@ -5,39 +5,45 @@ class Firm(abce.Agent):
     Firm in the economy
     have money which they exchange for labour
 
+    Accepts buy offers from random agents
+
     """
-    def init(self, money=10000, num_people=20, labour=0, produce=0):
+    def init(self, money=10000, num_people, labour=0, produce=0):
+        """creates a firm with 10000 money"""
         self.create("money", money)
         self.num_people = num_people
         self.labour = labour
         self.produce = produce
 
     def buy_labour(self, labour_cost):
+        """accepts sell offers of labour from a person agent"""
         for offer in self.get_offers("labour"):
             if offer.price <= self.not_reserved("money") and offer.price <= labour_cost:
                 self.accept(offer)
 
     def production(self):
+        """creates one produce and destroys one labour"""
         self.create("produce", self["labour"])
         self.destroy("labour", self["labour"])
 
 
     def sell_produce(self, produce_price):
+        """accepts buy offer of produce from all person agents"""
         for offer in self.get_offers("produce"):
             if offer.price >= produce_price and self["produce"] >= 1:
                 self.accept(offer)
 
     def print_possessions2(self):
+        """prints possessions and logs money belonging to a firm"""
         print('    ' + self.group + str(dict(self.possessions())))
         self.log("money", self["money"])
 
 
-#    def logger(self):
-#        self.log("v", 5)
-
     def getvalue(self):
+        """returns money belonging to firm"""
         return self["money"]
 
     def getvaluegoods(self):
+        """returns number of goods belonging to firm"""
         return self["produce"]
 
--- people.py
+++ people.py
@@ -15,26 +15,33 @@ class People(abce.Agent):
 
 
     def add_labour(self):
+        """adds one labour to person agent, usually called at start of day"""
         self.create("labour", 1)
 
 
     def sell_labour(self, labour_cost):
+        """sell offer to person agent's assigned firm"""
         self.sell(("firm", self.firm_number), good="labour", quantity=1, price=labour_cost)
 
     def buy_produce(self, cost):
+        """makes buy offer produce to a random firm"""
         while self.not_reserved("money") >= cost:
             self.buy(("firm", randrange(self.total_firms)), good="produce", quantity=1, price=cost)
 
     def destroy_labour(self):
+        """destroys all labour person agent owns"""
         self.destroy("labour", self.labour)
 
 
     def print_possessions(self):
+        """prints possessions and logs money of a person agent"""
         print('    ' + self.group + str(dict(self.possessions())))
         self.log("money", self["money"])
 
     def getvalue(self):
+        """returns the value of money owned by a person agent"""
         return self["money"]
 
     def getvaluegoods(self):
+        """returns the amount of produce owned by the person agent"""
         return self["produce"]
